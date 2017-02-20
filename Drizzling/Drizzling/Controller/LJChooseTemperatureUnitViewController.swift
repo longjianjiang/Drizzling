d7ecb7a8c6c2c01b47b67a16aad39d5440afa99b
longjianjiang <616393956@qq.com>
2017-02-20 22:22:05 +0800

    init project;

---
+++ Drizzling/Drizzling/Controller/LJChooseTemperatureUnitViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LJChooseTemperatureUnitViewController.swift
+//  Drizzling
+//
+//  Created by longjianjiang on 2017/2/8.
+//  Copyright © 2017年 Jiang. All rights reserved.
+//
+
+import UIKit
+
+class LJChooseTemperatureUnitViewController: UIViewController {
+    
+    @IBOutlet weak var msgLabel: UILabel!
+    @IBOutlet weak var celsiusBtn: UIButton!
+    @IBOutlet weak var fahrenheitBtn: UIButton!
+ 
+    
+    @IBAction func didClickBtn(_ sender: UIButton) {
+        let defaults = UserDefaults.standard
+
+        if sender == fahrenheitBtn {
+            defaults.set("F", forKey: "unit")
+            defaults.synchronize()
+            
+            print("FFFF")
+        } else if sender == celsiusBtn {
+            defaults.set("C", forKey: "unit")
+            defaults.synchronize()
+            print("CCCC")
+        }
+        
+        self.view.window?.rootViewController = ViewController()
+    }
+    
+    func getSuperAttibutedSring(_ string: NSString, and superString: String) -> NSMutableAttributedString {
+        let font:UIFont? = UIFont(name: "Helvetica", size:15)
+        let fontSuper:UIFont? = UIFont(name: "Helvetica", size:10)
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: string as String, attributes: [NSFontAttributeName:font!,NSForegroundColorAttributeName:UIColor.white])
+        let range = string.range(of: superString)
+        attString.setAttributes([NSFontAttributeName:fontSuper!,NSBaselineOffsetAttributeName:10,NSForegroundColorAttributeName:UIColor.white], range: range)
+        return attString
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        msgLabel.numberOfLines = 0
+        msgLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        msgLabel.text = "Which units would you like the\nweather to show"
+       
+        fahrenheitBtn.layer.cornerRadius = 20
+        fahrenheitBtn.layer.borderWidth = 1.0
+        fahrenheitBtn.layer.borderColor = UIColor.white.cgColor
+        fahrenheitBtn.setAttributedTitle(getSuperAttibutedSring("FAHRENHEIT oF", and: "o"), for: .normal)
+        
+        celsiusBtn.layer.cornerRadius = 20
+        celsiusBtn.layer.borderWidth = 1.0
+        celsiusBtn.layer.borderColor = UIColor.white.cgColor
+        celsiusBtn.setAttributedTitle(getSuperAttibutedSring("CELSIUS oC", and: "o"), for: .normal)
+    }
+
+    
+    deinit {
+        print("choose VC dealloc")
+    }
+}
---
+++ Drizzling/Drizzling/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Drizzling
+//
+//  Created by longjianjiang on 2017/2/7.
+//  Copyright © 2017年 Jiang. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
+    
+    lazy var geocoder: CLGeocoder = {
+        return CLGeocoder()
+    }()
+    
+    var currentCity: String! = nil
+    var currentCountry: String! = nil
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    // when the location is enabled and get the location info then will invoked.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        
+        guard let CurrentLocation = locations.last else {
+            return
+        }
+        
+        geocoder.reverseGeocodeLocation(CurrentLocation) { (places: [CLPlacemark]?, error: Error?) in
+            if error == nil { // normal condition
+                guard let pl = places?.first else {return}
+                self.currentCountry = pl.country
+                self.currentCity = pl.locality
+                print("current city is \(pl.locality), country is \(pl.country)")
+            } else { // no internet condition
+                let alertVC = UIAlertController(title: "Drizzling uses the internet to show the weather.\nAre you connected?", message: nil, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Got it", style: .default, handler: nil)
+                alertVC.addAction(okAction)
+                self.present(alertVC, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // when the location service not enabled will invoked.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let alertVC = UIAlertController(title: "Use the location show the weather where you are.", message: nil, preferredStyle: .alert)
+        
+        let openLocationAction = UIAlertAction(title: "Ok", style: .default) { (action: UIAlertAction) in
+            if let appSettings = URL(string: UIApplicationOpenSettingsURLString) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alertVC.addAction(openLocationAction)
+        alertVC.addAction(cancelAction)
+        
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        switch status {
+//        case .authorizedWhenInUse:
+//            manager.startUpdatingLocation()
+//        default:
+//            return
+//        }
+//    }
+    
+}
