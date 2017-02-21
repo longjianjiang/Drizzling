b5b1d9f392d01f9670f42e13e4229f7280ecbea3
jianglongjian <jianglongjian@corp-ci.com>
2017-02-21 10:37:49 +0800

    add framework

--- Drizzling/Drizzling/ViewController.swift
+++ Drizzling/Drizzling/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -30,6 +31,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         locationManager.startUpdatingLocation()
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
